b2671c77eb449e45bc8b34934748a439f09aaef7
Ziqi Wang <qiqiustc@gmail.com>
2023-03-30 12:18:21 +0200

    add bridge example

--- src/apps/benchmark/main.cpp
+++ src/apps/benchmark/main.cpp
@@ -7,13 +7,15 @@
 #include "sixbars_benchmark.h"
 #include "roboarch_benchmark.h"
 #include "tenbars_benchmark.h"
+#include "bridge_benchmark.h"
 int main()
 {
-    benchmark::SingleBar_BenchMark app;
+    //benchmark::SingleBar_BenchMark app;
     //benchmark::FourBars_BenchMark app;
     //benchmark::SixBars_BenchMark app;
     //benchmark::Roboarch_BenchMark app;
     //benchmark::TenBars_BenchMark app;
+    benchmark::Bridge_BenchMark app;
     app.launch();
     return 0;
 }
