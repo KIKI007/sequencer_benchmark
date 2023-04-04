7683509e512ae2c061ab894eb450d9cb50e93646
Ziqi Wang <qiqiustc@gmail.com>
2023-04-04 15:46:39 +0200

    add bunny

--- src/apps/examples/main.cpp
+++ src/apps/examples/main.cpp
@@ -7,15 +7,17 @@
 #include "roboarch_example.h"
 #include "frustum_example.h"
 #include "tower_example.h"
+#include "bunny_example.h"
 int main(){
     examples::Bridge_Example bridgeApp;
     examples::Roboarch_Example roboarchApp;
     examples::Fertility_Example fertilityApp;
     examples::Frustum_Example frustumApp;
     examples::Tower_Example towerApp;
+    examples::Bunny_Example bunnyApp;
     //fertilityApp.launch();
     //fertilityApp.read();
     //fertilityApp.render();
     //towerApp.launch();
-    roboarchApp.launch();
+    bunnyApp.launch();
 }
